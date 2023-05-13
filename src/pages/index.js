58f4e4c3b783286283c38c10364932915a37820c
Jonathon Toon <jonathontoon@gmail.com>
2023-05-13 13:47:02 +1200

    new layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,29 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {data ? JSON.stringify(data, null, 2) : "Loading..."}
+      {data ? (
+        <div className="grid grid-cols-3 gap-4">
+          {data.map((account) => (
+            <div
+              key={account.id}
+              className="bg-white rounded-lg p-6 shadow-md"
+            >
+              {Object.entries(account).map(([key, value]) => (
+                <p
+                  key={key}
+                  className={`text-gray-500 mb-2 ${
+                    key === "total_interest" ? "text-2xl font-bold" : ""
+                  }`}
+                >
+                  {key}: {value}
+                </p>
+              ))}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </main>
   );
-}
+};
